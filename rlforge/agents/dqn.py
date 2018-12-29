aac88455677855930977a809f96e65582a22231e
Avinash Ummadisingu <avinash.ummadisingu@usi.ch>
2018-12-29 23:21:13 +0100

    Added DoubleDQN implementation

--- rlforge/agents/dqn.py
+++ rlforge/agents/dqn.py
@@ -43,7 +43,7 @@ class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX,
         #DQN trains after every step so add it to the post_episode hook
         self.post_episode_hooks.append(self.learn)
 
-    def learn(self, global_step_ts:int , step_data:tuple):
+    def learn(self, global_step_ts , step_data):
         """Perform one step of learning with a batch of data
 
         post_step_hook Parameters:
@@ -51,10 +51,10 @@ class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX,
             step_data (tuple): (s,a,r,s',done)
         """
         states, actions, rewards, state_ns, dones = self.get_train_batch()
-        rewards, dones = np.float32(rewards), np.float32(dones)
+        rewards, is_not_terminal = np.float32(rewards), np.float32(np.invert(dones))
 
         q_star = tf.stop_gradient(tf.reduce_max(self.target_model(state_ns), axis=-1))
-        q_target = rewards+(self.gamma*q_star*(1.-dones))
+        q_target = rewards+(self.gamma*q_star*is_not_terminal)
 
         with tf.GradientTape() as tape:
             preds = self.model(states)
@@ -66,6 +66,54 @@ class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX,
         
         self.stats.append("step_losses",global_step_ts,losses)
         self.stats.append("step_mean_q",global_step_ts,np.mean(preds))
+
+
+
+class DoubleDQNAgent(DQNAgent):
+    """Double Deep-Q Learning Agent
+
+    Implementation of the Double DQN algorithm 
+    (https://arxiv.org/abs/1509.06461)
+
+
+    DQN Targets:
+    Y =  r + gamma * max_a(Q_target(s_new,a))
+
+    DoubleDQN Targets:
+    Y = r + gamma * Q_target(s_new, argmax_a(Q(s_new,a)))
+
+    """
+    def learn(self, global_step_ts , step_data):
+        """Perform one step of learning with a batch of data
+
+        post_step_hook Parameters:
+            global_step_ts (int)
+            step_data (tuple): (s,a,r,s',done)
+        """
+        states, actions, rewards, state_ns, dones = self.get_train_batch()
+        rewards, is_not_terminal = np.float32(rewards), np.float32(np.invert(dones))
+
+        best_sn_actions = np.argmax(self.model(state_ns), axis=-1)
+
+        target_preds = self.target_model(state_ns)
+        target_preds = tf.reduce_sum(target_preds * tf.one_hot(best_sn_actions, self.env.n_actions), axis=-1)
+
+
+        q_target = rewards+(self.gamma*target_preds*is_not_terminal)
+
+    
+
+        with tf.GradientTape() as tape:
+            preds = self.model(states)
+            q_cur = tf.reduce_sum(preds * tf.one_hot(actions, self.env.n_actions), axis=-1)
+            losses = tf.losses.huber_loss(labels=q_target, predictions=q_cur)
+            grads = tape.gradient(losses, self.model.trainable_weights)
+            self.opt.apply_gradients(zip(grads,
+                                       self.model.trainable_weights))
+        
+        self.stats.append("step_losses",global_step_ts,losses)
+        self.stats.append("step_mean_q",global_step_ts,np.mean(preds))
+
 
 
 if __name__ == '__main__':
