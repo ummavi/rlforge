1da7165fae8e187d13ae6b2bedeccb21da100c67
Avinash Ummadisingu <avinash.ummadisingu@usi.ch>
2018-12-29 13:45:36 +0100

    Added example for DQN

--- agents/dqn.py
+++ agents/dqn.py
@@ -2,24 +2,35 @@ import numpy as np
 import tensorflow as tf
 tf.enable_eager_execution()
 
-
-from rlforge.agents.base_agent import BaseAgent
-
 from rlforge.common.utils import Episode
+from rlforge.agents.base_agent import BaseAgent
 from rlforge.mixins.policies import EpsilonGreedyPolicyMX
 from rlforge.mixins.experience_replay import ExperienceReplayMX
 from rlforge.mixins.target_network import TargetNetworkMX
 
 
 
-class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX, TargetNetworkMX, BaseAgent):
-    """DQN
+class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX, 
+                TargetNetworkMX, BaseAgent):
+    """Deep-Q Network agent
+
+    Implementation of the DQN agent as described in the nature paper
+    (https://storage.googleapis.com/deepmind-media/dqn/DQNNaturePaper.pdf)
+
+
+    * Huber Loss
+    * Epsilon-greedy Policy (EpsilonGreedyPolicyMX)
+    * Experience Replay (ExperienceReplayMX)
+    * Uses Target Network to compute Q* for the target (TargetNetworkMX)
+
+
+    Note: Python inheritance from R-L. List BaseAgent at the end
     """
-    def __init__(self, env, q_fn, 
-                policy_learning_rate, replay_buffer_size, target_network_update_freq,
+    def __init__(self, env, q_function, policy_learning_rate, 
+                replay_buffer_size, target_network_update_freq,
                 minibatch_size = 32, gamma=0.98, eps=0.2):
 
-        self.model = q_fn
+        self.model = q_function
         self.gamma = gamma
 
         BaseAgent.__init__(self, env)
@@ -29,10 +40,10 @@ class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX, TargetNetworkMX, BaseA
 
         self.opt = tf.train.AdamOptimizer(policy_learning_rate)
 
-        #Since DQN Learns after every step.
-        self.post_step_hooks.append(self.learn)
+        #DQN trains after every step so add it to the post_episode hook
+        self.post_episode_hooks.append(self.learn)
 
-    def learn(self, global_step_ts, step_data):
+    def learn(self, global_step_ts:int , step_data:tuple):
         """Perform one step of learning with a batch of data
 
         post_step_hook Parameters:
@@ -53,4 +64,30 @@ class DQNAgent(EpsilonGreedyPolicyMX, ExperienceReplayMX, TargetNetworkMX, BaseA
             self.opt.apply_gradients(zip(grads,
                                        self.model.trainable_weights))
         return losses 
-    
+
+if __name__ == '__main__':
+    from tqdm import tqdm
+    from rlforge.common.q_functions import QNetwork
+    from rlforge.environments.environment import Gym_env
+
+    def train(agent, n_episodes):
+        train_returns = []
+        pbar = tqdm(range(n_episodes))
+        for i in pbar:
+            e = agent.interact(1)
+            train_returns.append(np.sum(e[0].rewards))
+            pbar.set_description("Latest return:"+str(train_returns[-1]))
+        return train_returns
+
+    np.random.seed(0)
+    tf.set_random_seed(0)   
+    env = Gym_env('CartPole-v0')
+    q_network = QNetwork(env.n_actions,env.d_observations,dict(layer_sizes=[24,24],
+                        activation="relu"))
+    agent = DQNAgent(env, q_network,policy_learning_rate=0.005,
+                    target_network_update_freq=200, 
+                    replay_buffer_size=5000,
+                    gamma=0.8,
+                    eps=0.2,
+                    minibatch_size=128)
+    train(agent,2500)
