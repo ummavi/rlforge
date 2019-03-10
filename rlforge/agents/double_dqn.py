b60c0a8ef53b25a67a087bd3051c9a6be9f8f067
Avinash Ummadisingu <u.avinash4x@gmail.com>
2019-03-10 14:54:47 +0530

    Ported Double-DQN to Chainer

--- rlforge/agents/double_dqn.py
+++ rlforge/agents/double_dqn.py
@@ -1,16 +1,13 @@
 import numpy as np
-import tensorflow as tf
 
+import chainer.functions as F
 from rlforge.agents.dqn import DQNAgent
-
-if not tf.executing_eagerly():
-    tf.enable_eager_execution()
-
+from rlforge.common.utils import one_hot
 
 class DoubleDQNAgent(DQNAgent):
     """Double Deep-Q Learning Agent
 
-    Implementation of the Double DQN algorithm 
+    Implementation of the Double DQN algorithm
     (https://arxiv.org/abs/1509.06461)
 
 
@@ -41,12 +38,12 @@ class DoubleDQNAgent(DQNAgent):
         best_sn_actions = np.argmax(self.network(state_ns), axis=-1)
 
         target_preds = self.target_network(state_ns)
-        target_preds = tf.reduce_sum(
-            target_preds * tf.one_hot(best_sn_actions, self.env.n_actions),
+        target_preds = F.sum(
+            target_preds * one_hot(best_sn_actions, self.env.n_actions),
             axis=-1)
 
         q_targets = rewards + (self.gamma * target_preds * is_not_terminal)
         preds, losses = self.network.update_q(states, actions, q_targets)
 
-        self.logger.log_scalar("step_losses", float(losses))
+        self.logger.log_scalar("step_losses", float(losses.array))
         self.logger.log_scalar("step_mean_q", np.mean(preds))
